6d6d912e9c1f70714054803fb258e37c29bfefc7
henry22 <henry811010@gmail.com>
2015-05-26 16:31:09 +0800

    We're going to isolate level loading into a single level, called loadLevel(). This needs to do two things: load and parse our level text file, then randomly assign letter groups to buttons.

--- Project8SevenSwiftyWords/ViewController.swift
+++ Project8SevenSwiftyWords/ViewController.swift
@@ -41,6 +41,41 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    func loadLevel() {
+        //store all the level's clues
+        var clueString = ""
+        //store how many letters each answer is
+        var solutionString = ""
+        //an array to store all letter groups
+        var letterBits = [String]()
+        
+        if let levelFilePath = NSBundle.mainBundle().pathForResource("level\(level)", ofType: "txt") {
+            if let levelContents = NSString(contentsOfFile: levelFilePath, usedEncoding: nil, error: nil) {
+                var lines = levelContents.componentsSeparatedByString("\n")
+                lines.shuffle()
+                
+                //enumerate() will place the item into the line variable and its position into the index variable
+                for (index, line) in enumerate(lines as! [String]) {
+                    let parts = line.componentsSeparatedByString(": ")
+                    let answer = parts[0]
+                    let clue = parts[1]
+                    
+                    //The array indexes start from 0, so add 1 to make it count from 1 to 7
+                    clueString += "\(index + 1). \(clue)\n"
+                    
+                    //Asking it to replace all instances of | with an empty string
+                    let solutionWord = answer.stringByReplacingOccurrencesOfString("|", withString: "")
+                    solutionString += "\(count(solutionWord)) letters\n"
+                    solutions.append(solutionWord)
+                    
+                    let bits = answer.componentsSeparatedByString("|")
+                    letterBits += bits
+                }
+            }
+        }
+        //Now configure the buttons and labels
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
